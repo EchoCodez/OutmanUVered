e4a1683132e4af423feb201d9c586a43a74495b0
EchoCodez <mihir.nimkar@gmail.com>
2023-03-04 10:48:58 -0500

    Deleted old stuff

--- src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Projects} from "./projects/Projects";
-export {default as Timeline} from "./timeline/Timeline";
-export {default as Footer} from "./footer/Footer";
-export {default as Header} from "./header/Header";
-export {default as Achievements} from "./achievements/Achievements";
-export {default as Technologies} from "./technologies/Technologies";
